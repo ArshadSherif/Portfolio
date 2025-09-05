1f13e0ca8b76c18deecb31f4d9db28e254edc489
Arshad <90891115+ArshadSherif@users.noreply.github.com.>
2025-09-06 02:30:59 +0530

    -Completed Projects Section -Fixed orbiting circles for large screens

--- src/app/sections/Projects/Projects.component.ts
+++ src/app/sections/Projects/Projects.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {myProjects} from '../../../constants/index';
 import { CommonModule } from '@angular/common';
 import { ProjectComponent } from "../../components/Project/Project.component";
+import gsap from 'gsap';
 @Component({
   selector: 'app-Projects',
   templateUrl: './Projects.component.html',
   styleUrls: ['./Projects.component.css'],
-  imports: [CommonModule, ProjectsComponent, ProjectComponent]
+  imports: [CommonModule, ProjectsComponent, ProjectComponent],
 })
 export class ProjectsComponent implements OnInit {
+  @ViewChild('follower') follower!: ElementRef<HTMLImageElement>;
 
-  myProjects:any = myProjects;
+  myProjects: any = myProjects;
+  hover_project: any = null;
+  hover_project_image: string = '';
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  ngAfterViewInit() { 
+      const el = this.follower.nativeElement;
+
+      // Create quickTo setters
+      const xTo = gsap.quickTo(el, 'x', { duration: 0.6, ease: 'power3' });
+      const yTo = gsap.quickTo(el, 'y', { duration: 0.6, ease: 'power3' });
+
+      // Make image follow mouse with smooth delay
+      window.addEventListener('mousemove', (e) => {
+        xTo(e.clientX -120); 
+        yTo(e.clientY + 20);
+      });
+
+  }
 }
